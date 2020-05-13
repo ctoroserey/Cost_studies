a2c6745e97c40701eadc86e8f7f1435d7201be42
Claudio Toro-Serey <ctoro@bu.edu>
2020-05-13 15:29:49 -0400

    new, simpler optimization function

--- Simulations/manuscript_modeling.R
+++ Simulations/manuscript_modeling.R
@@ -6,16 +6,43 @@
 # the idea being that you can enter a model and data, and the function will return the lowest LL and associated parameters
 # maybe also R-squares and things in the current optimization function
 
-negLogLik <- function(subjData, params, model) {
+
+
   
+negLogLik <- function(subjData, params, model) {
+  # this function finds the combination of parameter values that minimizes the neg log likelihood of a logistic regression
+  # used to rely on NLOPTR, but it's too cumbersome for the low-dimensional estimates I'm performing.
+  #
+  # subjData: a participant's log
+  # params: a list of vectors. Each vector is the possible values a given parameter can take. Names in list must match model expression
+  # model: using `expr()`, define the model (use <param>[[1]] for free parameters to be estimated. R limitation.)
+  
+  
+  # extract basic choice information
   handling <- subjData$Handling
   reward <- subjData$Offer
   choice <- subjData$Choice
+  rt <- subjData$RT
+  cost <- subjData$Cost
+  trial <- subjData$TrialN
   
-  LLs <- apply(params, 2, function(i) {
-    p = 1 / (1 + exp(-eval(model)))
+  # combine parameters into every possible combination
+  params <- expand.grid(params)
+  
+  LLs <- sapply(seq(nrow(params)), function(i) {
+    
+    # isolate the parameters for this iteration
+    # and then store them as variables
+    # FIGURE OUT HOW TO NOT STORE THEM AS DATAFRAMES
+    pars <- params_grid[i, ]
+    lapply(seq_along(pars), function(variable) {assign(colnames(pars)[variable], pars[variable], envir = .GlobalEnv)})
+    
+    # estimate the probability of acceptance per the model
+    p = 1 / (1 + exp(-eval(model_expr)))
     p[p == 1] <- 0.999
     p[p == 0] <- 0.001
+    
+    # get the likelihood of the observations based on the model
     tempChoice <- rep(NA, length(choice))
     tempChoice[choice == 1] <- log(p[choice == 1])
     tempChoice[choice == 0] <- log(1 - p[choice == 0]) # log of probability of choice 1 when choice 0 occurred
@@ -23,25 +50,29 @@ negLogLik <- function(subjData, params, model) {
   })
     
     
-  return(negLL)
+  return(LLs)
 }
 
 # model to be fit
-model_expr <- expr(params["temp"] * (reward - (params["gamma"] * handling)))
+# make sure that you specify the inverse temperature
+# extra parameters as dfs for now, that's why the `[[1]]`
+model_expr <- expr(temp[[1]] * (reward - (gamma[[1]] * handling)))
 
 # create a list with possible starting values for model parameters
 params <- list(temp = seq(-1, 1, length.out = 100), 
                gamma = seq(0.25, 1.5, length.out = 100))
 
 # combine into every possible combination
-params_grid <- as.matrix(expand.grid(params))
+params_grid <- expand.grid(params)
 
 subj <- 190
-Data <- dataBtw %>%
+subjData <- dataBtw %>%
   filter(SubjID == subj)
 
+test <- negLogLik(subjData, params_grid, model_expr)
 
-t <- sapply(seq(nrow(params_grid)), function(i) negLogLik(params_grid[i, ], model_expr, choice, handling, reward))
+
+
 
 # plot the likelihood over the parameter space (surprisingly convex)
 plot(t)
